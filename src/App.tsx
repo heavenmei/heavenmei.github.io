eb75916963902ee850ea5c0ccc04c53bcc5f97ea
heavenmei <1394003825@qq.com>
2024-01-23 19:52:58 +0800

    fix: bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import Post from "./pages/post";
 
 function App() {
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
+import "./assets/css/main.css";
 import "./assets/css/github-markdown.css";
 import "markdown-navbar/dist/navbar.css";
 
