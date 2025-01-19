a93f251d4d73112507859049aa7f222e78e81cc4
heavenmei <1394003825@qq.com>
2025-01-19 19:33:06 +0800

    add proj

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -59,12 +59,25 @@ const config = {
   ],
   projectList: [
     {
-      tag: "CCF A",
-      img: "/images/2024_salientime.jpg",
-      title: `SalienTime: User-driven Selection of Salient Time Steps for Large-Scale Geospatial Data Visualization`,
-      venue: `ACM SIGCHI Conference on Human Factors in Computing Systems 2024 (CHI 2024)`,
-      authors: `Juntong Chen, Haiwen Huang, Huayuan Ye, Zhong Peng, Chenhui Li, Changbo Wang`,
-      desc: `Selecting a subset of time steps with user-specified priorities, leveraging structural variation info learned by Autoencoders.`,
+      tag: "WebGL",
+      img: "/images/fluid.png",
+      title: "艺术流体可视化",
+      authors: `Haiwen Huang`,
+      desc: `利用WebGL技术，实现了艺术流体可视化。`,
+    },
+    {
+      tag: "Paper",
+      img: "/images/explanationVis.png",
+      title: "提升用户信任度的机器学习解释框架",
+      authors: `Haiwen Huang, Yin Tang`,
+      desc: `通过增强型事实检查框架对任务模型进行解释分析处理。利用偏相关方差算法、锚定算法、影响函数、反事实算法等多种算法对模型及训练集进行处理，进一步加深了对任务模型的理解。将处理结果输入到交互式机器学习模型解释可视化分析系统中，对模型进行深入分析.`,
+    },
+    {
+      tag: "Vis",
+      img: "/images/atmosphereVis.png",
+      title: "大气污染可视化平台",
+      authors: `Haiwen Huang`,
+      desc: `2021年可视化分析大赛.基于大气污染数据集，设计了一套大气污染可视化平台.`,
     },
   ],
 };
